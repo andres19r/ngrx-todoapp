08c940b343404f12bfc816038a6c1dfa2c764f44
Andres Rivero <andres19rivero@gmail.com>
2023-01-15 19:41:19 -0400

    feat: retrieve todos from store in list component

--- src/app/todos/todo.reducer.ts
+++ src/app/todos/todo.reducer.ts
@@ -3,7 +3,10 @@ import { Todo } from './models/todo.model';
 import { create } from './todo.actions';
 
 export const initialState: Todo[] = [
-    new Todo('Save the world')
+    new Todo('Save the world'),
+    new Todo('Defeat Thanos'),
+    new Todo('Buy Iron Man suit'),
+    new Todo('Stole the Caps shield'),
 ];
 
 export const todoReducer = createReducer(
