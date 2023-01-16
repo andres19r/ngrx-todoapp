a2b1da2266cfb60f9a50bb185e8f3962f13162b4
Andres Rivero <andres19rivero@gmail.com>
2023-01-15 20:23:30 -0400

    bind todo-item elements & behaviors

--- src/app/todos/todo-item/todo-item.component.ts
+++ src/app/todos/todo-item/todo-item.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Todo } from '../models/todo.model';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.css']
+  styleUrls: ['./todo-item.component.css'],
 })
-export class TodoItemComponent {
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo = {} as Todo;
+  @ViewChild('inputRef') txtInputRef: ElementRef = {} as ElementRef;
+  chkCompleted: FormControl = new FormControl(false);
+  txtInput: FormControl = new FormControl('', Validators.required);
+  editing: boolean = false;
 
+  constructor() {}
+
+  ngOnInit() {
+    this.chkCompleted = new FormControl(this.todo.completed);
+    this.txtInput = new FormControl(this.todo.text, Validators.required);
+  }
+
+  edit() {
+    this.editing = true;
+    setTimeout(() => {
+      this.txtInputRef.nativeElement.select();
+    }, 1);
+  }
+
+  finishEdit() {
+    this.editing = false;
+  }
 }
