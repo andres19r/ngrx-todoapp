51a082c3fa7f3e5a1714165a36b8d34d584b4477
Andres Rivero <andres19rivero@gmail.com>
2023-01-19 22:31:48 -0400

    generate filters from store and change filter with action

--- src/app/todos/todo-footer/todo-footer.component.ts
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import * as actions from 'src/app/filter/filter.actions';
 
 @Component({
   selector: 'app-todo-footer',
   templateUrl: './todo-footer.component.html',
-  styleUrls: ['./todo-footer.component.css']
+  styleUrls: ['./todo-footer.component.css'],
 })
-export class TodoFooterComponent {
+export class TodoFooterComponent implements OnInit {
+  actualFilter: actions.validFilters = 'all';
+  filters: actions.validFilters[] = ['all', 'completed', 'pending'];
 
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit() {
+    this.store
+      .select('filter')
+      .subscribe((filter) => (this.actualFilter = filter));
+  }
+
+  changeFilter(filter: actions.validFilters) {
+    this.store.dispatch(actions.setFilter({ filter }));
+  }
 }
